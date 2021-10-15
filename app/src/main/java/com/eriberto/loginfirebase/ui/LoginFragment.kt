58459e663f7f493b5b54de923caf6beeeca59ffb
laeriberto.junior <eriberto.junior@thoughtworks.com>
2021-10-15 11:06:23 -0300

    configuracao de nav_graph.xml e tela de LoginFragment.kt criada

--- app/src/main/java/com/eriberto/loginfirebase/ui/LoginFragment.kt
+++ app/src/main/java/com/eriberto/loginfirebase/ui/LoginFragment.kt
@@ -1,13 +1,17 @@
 package com.eriberto.loginfirebase.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.eriberto.loginfirebase.R
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.eriberto.loginfirebase.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,11 +21,28 @@ class LoginFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        carregarImagem()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun carregarImagem() {
+        val imageView = binding.imageView
+        context?.let {
+            Glide.with(it)
+                .load("https://rockcontent.com/br/wp-content/uploads/sites/2/2019/04/firebase.png")
+                .circleCrop()
+                .into(imageView)
+        }
     }
 }
