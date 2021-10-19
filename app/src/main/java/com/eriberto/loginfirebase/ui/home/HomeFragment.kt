b1df3ae09522f4b24ed43d5538df30fdb0fb8636
laeriberto.junior <eriberto.junior@thoughtworks.com>
2021-10-19 18:13:50 -0300

    adicionado animacao em ImageView compartilhado

--- app/src/main/java/com/eriberto/loginfirebase/ui/home/HomeFragment.kt
+++ app/src/main/java/com/eriberto/loginfirebase/ui/home/HomeFragment.kt
@@ -5,17 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.ViewCompat
+import androidx.navigation.fragment.FragmentNavigatorExtras
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.eriberto.loginfirebase.databinding.FragmentHomeBinding
+import com.eriberto.loginfirebase.ui.login.LoginFragment.Companion.CHAVE_IMAGEM_LOGIN_LOGO
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private val controlador by lazy { findNavController() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,10 +30,24 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         carregarImagem()
+        configuraBotaoLoginEmail()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun configuraBotaoLoginEmail() {
+        binding.loginEmail.setOnClickListener {
+            val extras = FragmentNavigatorExtras(binding.ivLogo to CHAVE_IMAGEM_LOGIN_LOGO)
+            val direcao = HomeFragmentDirections.actionHomeFragmentToLoginFragment()
+            controlador.navigate(direcao, extras)
+        }
     }
 
     private fun carregarImagem() {
-        val imageView = binding.imageView
+        val imageView = binding.ivLogo
         context?.let {
             Glide.with(it)
                 .load("https://rockcontent.com/br/wp-content/uploads/sites/2/2019/04/firebase.png")
--- app/src/main/java/com/eriberto/loginfirebase/ui/login/LoginFragment.kt
+++ app/src/main/java/com/eriberto/loginfirebase/ui/login/LoginFragment.kt
@@ -1,17 +1,32 @@
 package com.eriberto.loginfirebase.ui.login
 
 import android.os.Bundle
+import android.transition.TransitionInflater
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.eriberto.loginfirebase.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
+    companion object {
+        const val CHAVE_IMAGEM_LOGIN_LOGO = "login_logo"
+    }
+
     private var _binding: FragmentLoginBinding? = null
+
     private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val animacao =
+            TransitionInflater.from(context).inflateTransition(android.R.transition.move)
+        sharedElementEnterTransition = animacao
+        sharedElementReturnTransition = animacao
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,6 +38,7 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        ViewCompat.setTransitionName(binding.ivLogo, CHAVE_IMAGEM_LOGIN_LOGO)
         carregarImagem()
     }
 
@@ -32,7 +48,7 @@ class LoginFragment : Fragment() {
     }
 
     private fun carregarImagem() {
-        val imageView = binding.imageView
+        val imageView = binding.ivLogo
         context?.let {
             Glide.with(it)
                 .load("https://rockcontent.com/br/wp-content/uploads/sites/2/2019/04/firebase.png")
